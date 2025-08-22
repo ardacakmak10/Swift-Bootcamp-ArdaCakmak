a359ff184a61c01d350f15613fe260afbfb8681b
Arda Çakmak <120424496+ardacakmak10@users.noreply.github.com>
2025-08-22 20:40:54 +0300

    init new project

---
+++ KullaniciProfilKarti/KullaniciProfilKarti/KullaniciProfilKartiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KullaniciProfilKartiApp.swift
+//  KullaniciProfilKarti
+//
+//  Created by Arda Çakmak on 22.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct KullaniciProfilKartiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KullaniciProfilKarti/KullaniciProfilKartiTests/KullaniciProfilKartiTests.swift
@@ -0,0 +1,17 @@
+//
+//  KullaniciProfilKartiTests.swift
+//  KullaniciProfilKartiTests
+//
+//  Created by Arda Çakmak on 22.08.2025.
+//
+
+import Testing
+@testable import KullaniciProfilKarti
+
+struct KullaniciProfilKartiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
